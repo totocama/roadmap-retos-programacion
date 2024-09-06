0e5d77d71b6e7ea4e3f32726ca203ffb361ca92a
totocama <tomasaugustocama@gmail.com>
2024-09-06 17:52:11 -0300

    Create totocama.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/totocama.py
@@ -0,0 +1,25 @@
+#https://www.python.org/
+
+#Comentario en una linea 
+
+"""
+Esto es un 
+comentario en 
+varias lineas
+"""
+
+
+my_variable = "Mi variable"
+my_variable = "Mi nueva variavle"
+
+MY_CONSTANT = "Mi constante" # por convención 
+MY_CONSTANT = "matecitos"
+
+my_int = 1 
+my_float = 1.4
+my_bool = True 
+my_bool = False 
+my_string = "mi cadena de texto"
+my_other_string = "mi cadena de texto distinta"
+
+print("!Hola, Phyton")   
